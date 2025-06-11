c9caa20efb325f78c79250e150addf5ec3876d4b
lizzybera <elizabethokorie407@gmail.com>
2025-06-11 21:03:35 +0100

    typescript done

---
+++ second.ts
@@ -0,0 +1,11 @@
+import {func, car} from "./index"
+console.log("Second");
+
+const myCar : car = {
+    name : "BMW",
+    year : 4042
+}
+
+console.log(func(30, 10));
+console.log("");
+console.log(myCar);
